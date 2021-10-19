f6cbc9c83f5699e2d5d2702b2dae9beacea29d7c
nmitchell313 <nmitchel11@cnm.edu>
2021-10-19 14:22:36 -0600

    added hello world.

--- src/main/java/edu/cnm/deepdive/dicewareservice/controller/HelloWorldController.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.dicewareservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("greet")
-    public String greet(String target) {
+    public String greet(@RequestParam(name = "t", required = false, defaultValue = "World!") String target) {
         return String.format("Hello, %s", target);
 
+    }
+
+    @GetMapping("goodjob")
+    public String goodJob() {
+        return "You're building a webserver then soon onto a webservice, congratulations!";
     }
 }
