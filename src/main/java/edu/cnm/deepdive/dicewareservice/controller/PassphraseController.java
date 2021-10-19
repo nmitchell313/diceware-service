21d66a188fb6ef424a07732ead0fe145a61a084e
nmitchell313 <nmitchel11@cnm.edu>
2021-10-19 15:46:52 -0600

    added diceware interfaces.

--- src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.dicewareservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import edu.cnm.deepdive.dicewareservice.service.PassphraseGenerator;
@@ -17,7 +18,8 @@ public class PassphraseController {
         this.generator = generator;
     }
 
+    @GetMapping("/transient")
     public String[] generate(int length) {
-
+        return generator.generate(length);
     }
 }
