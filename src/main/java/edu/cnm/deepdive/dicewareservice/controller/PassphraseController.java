5af644541633729f670d9cfae1f3ab3c65a3cc83
nmitchell313 <nmitchel11@cnm.edu>
2021-10-19 15:39:54 -0600

    service package moved.

---
+++ src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
@@ -0,0 +1,23 @@
+package edu.cnm.deepdive.dicewareservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import edu.cnm.deepdive.dicewareservice.service.PassphraseGenerator;
+
+@RestController
+@RequestMapping("/passphrases")
+public class PassphraseController {
+
+    private final PassphraseGenerator generator;
+
+    @Autowired
+    public PassphraseController(
+            PassphraseGenerator generator) {
+        this.generator = generator;
+    }
+
+    public String[] generate(int length) {
+
+    }
+}
---
+++ src/main/java/edu/cnm/deepdive/dicewareservice/service/PassphraseGenerator.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.dicewareservice.service;
+
+public interface PassphraseGenerator {
+
+    String[] generate(int length);
+
+}
